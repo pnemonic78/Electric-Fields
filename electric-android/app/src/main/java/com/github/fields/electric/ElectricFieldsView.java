3d4e7940978b03e0661400a4529fcaaa9e92fc5b
Moshe W <moshe.w@invest.com>
2017-01-29 21:35:47 +0200

    plot some charges.

--- electric-android/app/src/main/java/com/github/fields/electric/ElectricFieldsView.java
+++ electric-android/app/src/main/java/com/github/fields/electric/ElectricFieldsView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.view.View;
@@ -71,86 +72,6 @@ public class ElectricFieldsView extends View {
         canvas.drawBitmap(bitmap, 0, 0, null);
     }
 
-    protected void plot(int x, int y) {
-//        int c;
-//        double v = 0;
-//        double z;
-//        double r, xs, ys;
-//        int h, w;
-//        int i = 1;
-//        boolean overflow = false;
-//        Charge charge;
-//
-//        do {
-//            charge = charges.get(i);
-//            xs = (x - charge.x);
-//            xs = xs * (x - charge.x);
-//            ys = (y - charge.y);
-//            ys = ys * (y - charge.y);
-//            r = xs + ys;
-//            r = Math.sqrt(r);
-//            if (r == 0) {
-//                overflow = true;
-//                break;
-//            } else {
-//                v = v + (charge.size / r);
-//            }
-//
-//            i++;
-//        } while (i <= charges.size());
-//
-//        if (!overflow) {
-//            z = (v + 1) * (PY + 1);
-////            z = MaxColor * ((z / MaxColor) - Round(z / MaxColor));
-//            c = RGBFilter(Round(z));
-//            w = Round(Res);
-//            h = Round(Res);
-////        with Form1.Image1.Canvas do
-////            begin
-////                    Lock;
-////        Brush.Style:=bsSolid;
-////        Brush.Color:=c;
-////        Pen.Color:=c;
-////        if Res > 1 then
-////        Rectangle(x, y, x + w, y + h)
-////        else
-////        Pixels[x, y]:=c;
-////        Unlock;
-//        }
-    }
-
-    private int RGBFilter(int c) {
-        return 0xFF000000 | (c & 0xFFFFFF);
-    }
-
-    private void calculateField() {
-//
-//        if (PX >= PY)
-//            Res = PX / 2;
-//        else
-//            Res = PY / 2;
-//
-//        plot(0, 0);
-//
-//        double x;
-//        double y = 0;
-//        do {
-//            x = Res;
-//            do {
-//                plot(Round(x), Round(y));
-//                plot(Round(x - Res), Round(y + Res));
-//                plot(Round(x), Round(y + Res));
-//                postInvalidate();
-//
-//                x = x + (Res * 2);
-////    Application.ProcessMessages;
-//            } while (x <= PX);
-//            y = y + (Res * 2);
-////    Application.ProcessMessages;
-//        } while (y >= PY);
-//        Res = Res / 2;
-    }
-
     public void start() {
         cancel();
         task = new FieldAsyncTask().execute(bitmap);
@@ -162,10 +83,6 @@ public class ElectricFieldsView extends View {
         }
     }
 
-    private static int Round(double a) {
-        return (int) Math.round(a);
-    }
-
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
@@ -175,6 +92,8 @@ public class ElectricFieldsView extends View {
     private class FieldAsyncTask extends AsyncTask<Bitmap, Bitmap, Bitmap> {
 
         private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        private int resolution;
+        private final RectF rect = new RectF();
 
         @Override
         protected void onPreExecute() {
@@ -190,13 +109,38 @@ public class ElectricFieldsView extends View {
             Bitmap bitmap = params[0];
             bitmap.eraseColor(Color.WHITE);
 
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
+            resolution = Math.min(w, h) / 2;
+            double resolution2 = resolution * 2;
+
             Canvas bitmapCanvas = new Canvas(bitmap);
-            paint.setColor(Color.RED);
-            float x1 = bitmap.getWidth() - 100;
-            float y1 = bitmap.getHeight() - 100;
-            float x2 = bitmap.getWidth();
-            float y2 = bitmap.getHeight();
-            bitmapCanvas.drawRect(x1, y1, x2, y2, paint);
+            plot(bitmapCanvas, 0, 0);
+
+            int x;
+            int y = 0;
+            do {
+                do {
+                    x = resolution;
+                    do {
+                        plot(bitmapCanvas, x, y);
+                        plot(bitmapCanvas, x - resolution, y + resolution);
+                        plot(bitmapCanvas, x, y + resolution);
+                        postInvalidate();
+
+                        x += resolution2;
+                    } while (x < w);
+                    y += resolution2;
+                } while (y < h);
+
+                try {
+                    Thread.sleep(500L);//TODO delay is from settings.
+                } catch (InterruptedException e) {
+                }
+                resolution = resolution / 2;
+                resolution2 = resolution * 2;
+            } while (resolution > 1);
+
             return bitmap;
         }
 
@@ -211,6 +155,50 @@ public class ElectricFieldsView extends View {
             super.onPostExecute(result);
             invalidate();
             Toast.makeText(getContext(), "Finished.", Toast.LENGTH_SHORT).show();
+        }
+
+        protected void plot(Canvas canvas, int x, int y) {
+            int c;
+            double v = 0;
+            double z;
+            double r, dx, dy;
+            int h, w;
+            boolean overflow = false;
+
+            for (Charge charge : charges) {
+                dx = x - charge.x;
+                dy = y - charge.y;
+                r = Math.sqrt((dx * dx) + (dy * dy));
+                if (r == 0) {
+                    overflow = true;
+                    break;
+                }
+                v += charge.size / r;
+            }
+
+            if (!overflow) {
+                int bh = canvas.getHeight();
+                z = (v + 1) * (bh + 1);
+                //TODO z = MaxColor * ((z / MaxColor) - Round(z / MaxColor));
+                c = filterColor(Round(z));
+                w = resolution;
+                h = resolution;
+
+                paint.setColor(c);
+                rect.set(x, y, x + w, y + h);
+                canvas.drawRect(rect, paint);
+            }
+        }
+
+        private int filterColor(int c) {
+            int r = (c & 0xF00) >> 4;
+            int g = (c & 0x0F0);
+            int b = (c & 0x00F) << 4;
+            return Color.rgb(r, g, b);
+        }
+
+        private int Round(double a) {
+            return (int) Math.round(a);
         }
     }
 }
