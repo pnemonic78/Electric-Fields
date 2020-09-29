ab8b920caf262f1beea5c5af3be7a9a7666794f4
M. Waisberg <pnemonic@gmail.com>
2020-09-29 12:42:09 +0300

    migrate groovy to kts

---
+++ electric-android/buildSrc/src/main/kotlin/Depends.kt
@@ -0,0 +1,6 @@
+object Extra {
+    const val kotlinVersion = "1.4.10"
+    const val androidBuildSdkVersion = 29
+    const val androidBuildMinSdkVersion = 21
+    const val androidBuildTargetSdkVersion = 29
+}
