1156507b09303af601ebe28f5c6198b2698d55a5
M. Waisberg <pnemonic@gmail.com>
2023-08-20 08:42:06 +0300

    latest dependencies

--- electric-android/app/build.gradle.kts
+++ electric-android/app/build.gradle.kts
@@ -7,12 +7,12 @@ val versionMajor = (project.property("APP_VERSION_MAJOR") as String).toInt()
 val versionMinor = (project.property("APP_VERSION_MINOR") as String).toInt()
 
 android {
-    compileSdkVersion(Versions.androidBuildSdkVersion)
+    compileSdk = Versions.androidBuildSdkVersion
 
     defaultConfig {
         applicationId = "com.github.fields.electric"
-        minSdkVersion(Versions.androidBuildMinSdkVersion)
-        targetSdkVersion(Versions.androidBuildTargetSdkVersion)
+        minSdk = Versions.androidBuildMinSdkVersion
+        targetSdk = Versions.androidBuildTargetSdkVersion
         versionCode = generateVersionCode(versionMajor, versionMinor)
         versionName = "${versionMajor}.${versionMinor.toString().padLeft(2, "0")}"
 
@@ -67,15 +67,12 @@ android {
 }
 
 dependencies {
-    // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}")
-
     // Rx
-    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
 
     // AndroidX
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
--- electric-android/buildSrc/src/main/kotlin/Depends.kt
+++ electric-android/buildSrc/src/main/kotlin/Depends.kt
@@ -1,6 +1,6 @@
 object Versions {
-    const val kotlinVersion = "1.4.32"
-    const val androidBuildSdkVersion = 31
+    const val kotlinVersion = "1.8.20"
+    const val androidBuildSdkVersion = 33
     const val androidBuildMinSdkVersion = 21
-    const val androidBuildTargetSdkVersion = 31
+    const val androidBuildTargetSdkVersion = 33
 }
